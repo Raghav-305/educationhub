5bfbeb7492e97e076823580027c0177311101198
Raghav <raghav.bhardwaj.305@gmail.com>
2026-03-01 17:21:52 +0530

    full project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import EducationHub from "./EducationHub";
+
+function App() {
+  return (
+    <div>
+      <EducationHub />
+    </div>
+  );
+}
+
+export default App;
